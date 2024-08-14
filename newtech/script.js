2c2dcb6e627118fd2112a53da2677f35b866b328
Ravishka Bhashitha <67957355+ravishka22@users.noreply.github.com>
2024-08-14 17:14:41 +0530

    Add files via upload

---
+++ newtech/script.js
@@ -0,0 +1,7 @@
+function changeView(){
+    var signUpBox = document.getElementById("signupbox");
+    var signInBox = document.getElementById("signinbox");
+
+    signUpBox.classList.toggle("d-none");
+    signInBox.classList.toggle("d-none");
+}
