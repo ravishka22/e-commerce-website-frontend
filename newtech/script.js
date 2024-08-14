36c71ee97e8911cc5a104055195aa22128a4d6c6
Ravishka Bhashitha <67957355+ravishka22@users.noreply.github.com>
2024-08-14 17:16:06 +0530

    Delete newtech directory

--- newtech/script.js
+++
@@ -1,7 +0,0 @@
-function changeView(){
-    var signUpBox = document.getElementById("signupbox");
-    var signInBox = document.getElementById("signinbox");
-
-    signUpBox.classList.toggle("d-none");
-    signInBox.classList.toggle("d-none");
-}
